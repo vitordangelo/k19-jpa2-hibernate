07ffe9c0e1ed65bfb98066a196ef6eee15dee8d9
vitor <metroid.p2p@gmail.com>
2014-09-23 15:05:30 -0300

    Exercício OneToOne Apostila

---
+++ OneToOne/src/main/java/br/com/k19/teste/AdicionaEstadoGovernador.java
@@ -0,0 +1,35 @@
+package br.com.k19.teste;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.k19.modelo.Estado;
+import br.com.k19.modelo.Governador;
+
+public class AdicionaEstadoGovernador {
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory("K21_mapeamento_pu");
+		EntityManager manager = factory.createEntityManager();
+
+		manager.getTransaction().begin();
+		
+		Governador g = new Governador();
+		g.setNome("Rafael Cosentino");
+		
+		Estado e = new Estado();
+		e.setNome("São Paulo");
+		e.setGovernador(g);
+		
+		manager.persist(g);
+		manager.persist(e);
+
+		manager.getTransaction().commit();
+		
+		manager.close();
+		factory.close();
+
+	}
+}
