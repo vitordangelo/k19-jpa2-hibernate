b7e23fdbfca7d16b51d60cbd3b3fd9db73f55d51
vitor <metroid.p2p@gmail.com>
2014-09-24 11:21:28 -0300

    OneToMany

---
+++ OneToMany/src/main/java/br/com/k19/modelo/Departamento.java
@@ -0,0 +1,46 @@
+package br.com.k19.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Departamento {
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String nome;
+
+	@OneToMany
+	private List<Funcionario> funcionarios = new ArrayList<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+
+}
