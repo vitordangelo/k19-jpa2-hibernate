13246c63d3c82e523f08c3f641772dec3b720a2d
vitor <metroid.p2p@gmail.com>
2014-09-24 11:55:57 -0300

    ManyToMany

---
+++ ManyToMany/src/main/java/br/com/k19/modelo/Livro.java
@@ -0,0 +1,46 @@
+package br.com.k19.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Livro {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String nome;
+
+	@ManyToMany
+	private List<Autor> autores = new ArrayList<Autor>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Autor> getAutores() {
+		return autores;
+	}
+
+	public void setAutores(List<Autor> autores) {
+		this.autores = autores;
+	}
+
+}
